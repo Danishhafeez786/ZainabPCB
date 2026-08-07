fe90ad58ca5406df904d7a86d9d1b461a7d4b3d4
Danish Hafeez <devmastersinstitue@gmail.com>
2026-08-07 18:25:11 +0500

    style: Refactor code formatting in AppRouter and vite.config for consistency

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter,} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "../components/layout/MainLayout";
 
@@ -11,27 +11,26 @@ import Contact from "../pages/Contact";
 import HybridCourse from "../pages/courses/HybridCourse";
 import ACPcbCourse from "../pages/courses/ACPcbCourse";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "services", element: <Services /> },
+        { path: "gallery", element: <Gallery /> },
+        { path: "feedback", element: <Feedback /> },
+        { path: "contact", element: <Contact /> },
+        { path: "courses/hybrid", element: <HybridCourse /> },
+        { path: "courses/ac-pcb", element: <ACPcbCourse /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "services", element: <Services /> },
-      { path: "gallery", element: <Gallery /> },
-      { path: "feedback", element: <Feedback /> },
-      { path: "contact", element: <Contact /> },
-      {
-        path: "courses/hybrid",
-        element: <HybridCourse />,
-      },
-      {
-        path: "courses/ac-pcb",
-        element: <ACPcbCourse />,
-      },
-    ],
-  },
-]);
+    basename: "/ZainabPCB",
+  }
+);
 
 export default router;
